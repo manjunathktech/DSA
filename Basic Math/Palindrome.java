5c72714c1b47a6c7111d0bbaed6b7e7604402d38
manjunath kattimani <Manjunathkattimani7411@gmail.com>
2026-07-25 01:00:40 +0530

    Implement palindrome check in Palindrome.java
    Add a Java program to check if a number is a palindrome.

--- Basic Math/Palindrome.java
+++ Basic Math/Palindrome.java
@@ -1,1 +1,24 @@
 
+import java.util.Scanner;
+public class Solution {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int dup = n;
+		int revNum = 0;
+		while(n>0){
+			int ld = n%10;
+			revNum = (revNum*10) + ld;
+			n = n/10;
+		}
+		if(dup==revNum){
+			System.out.println("true");
+		}
+		else{
+			System.out.println("false");
+		}
+
+		
+	}
+}
