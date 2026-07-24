c5743cc1a377a96e4a729210abc13f448a8f19e7
manjunath kattimani <Manjunathkattimani7411@gmail.com>
2026-07-24 21:54:28 +0530

    Update NoOfDigits9173.java

--- Basic Math/NoOfDigits9173.java
+++ Basic Math/NoOfDigits9173.java
@@ -1,1 +1,14 @@
+public class Solution {
+    public static int countDigits(int n){
+     int cnt = 0;
+     while(n > 0){
+         cnt = cnt + 1;
+         n = n / 10;
+     }
+     return cnt;
+    }
+}
 
+//if we find through the logarithmic 
+int cnt = (int)(log10(n) + 1);
+     return cnt;
