2c011b1995e6d6f4d32ccef8794c101c5749ef8b
manjunath kattimani <Manjunathkattimani7411@gmail.com>
2026-07-25 00:01:11 +0530

    Implement number reversal in RevereseOfaNumber.java

--- Basic Math/RevereseOfaNumber.java
+++ Basic Math/RevereseOfaNumber.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+		int revNum = 0;
+		while(n>0){
+			int ld = n % 10;
+			revNum = (revNum * 10) + ld;
+			n = n/10;
+		}
+		System.out.println(revNum);
+
+
+	}
+}
 
